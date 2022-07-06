3e9ab2a6efa06ce55805dbc56b79be6493528c6c
gdavidss <davidguilherme.david@gmail.com>
2022-07-06 15:48:50 -0700

    Added tab view controller, with chat list VC

---
+++ ProjectVoyage/View Controllers/ChatListVC.h
@@ -0,0 +1,16 @@
+//
+//  ChatListVC.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatListVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
