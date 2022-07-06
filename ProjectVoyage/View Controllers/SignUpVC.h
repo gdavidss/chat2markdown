8a35326b664d32263222f01240559c5b1cd36655
gdavidss <davidguilherme.david@gmail.com>
2022-07-06 14:15:00 -0700

    Implemented skeleton of sign up page

---
+++ ProjectVoyage/View Controllers/SignUpVC.h
@@ -0,0 +1,16 @@
+//
+//  SignUpVC.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
