92da983eaf7a6733931179e14544e32c0f822319
gdavidss <davidguilherme.david@gmail.com>
2022-07-06 13:05:43 -0700

    Implemented skeleton of login

---
+++ ProjectVoyage/View Controllers/LoginVC.h
@@ -0,0 +1,16 @@
+//
+//  LoginVC.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
