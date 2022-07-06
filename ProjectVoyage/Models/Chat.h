cd593d188f8b5d3ec3f15e0e923bc1cc5c17fc76
gdavidss <davidguilherme.david@gmail.com>
2022-07-06 16:59:36 -0700

    Added name field in sign-up

---
+++ ProjectVoyage/Models/Chat.h
@@ -0,0 +1,16 @@
+//
+//  Chat.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Chat : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ProjectVoyage/Models/Chat.m
@@ -0,0 +1,12 @@
+//
+//  Chat.m
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import "Chat.h"
+
+@implementation Chat
+
+@end
