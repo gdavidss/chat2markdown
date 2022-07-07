9b644bfde8c41edf1f86893eccf7a8a8dd146b01
gdavidss <davidguilherme.david@gmail.com>
2022-07-06 17:18:35 -0700

    Displaying user data on settings

---
+++ ProjectVoyage/View Controllers/SettingsVC.h
@@ -0,0 +1,16 @@
+//
+//  SettingsVC.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
