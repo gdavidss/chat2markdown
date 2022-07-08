3bbcb96d504a38e71b063d9053898dd0fed709b5
gdavidss <davidguilherme.david@gmail.com>
2022-07-08 14:30:25 -0700

    Added assets

---
+++ ProjectVoyage/View Controllers/ChatMessagesVC.h
@@ -0,0 +1,16 @@
+//
+//  ChatMessagesVC.h
+//  ProjectVoyage
+//
+//  Created by Gui David on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatMessagesVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
